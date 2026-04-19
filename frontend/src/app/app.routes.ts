e48c5ecf1ffc7ad47799b926b9c49ef3d4da1907
unknown <Admin@IT-Antor.bdjobs.local>
2026-04-19 09:59:23 +0600

    simplified path

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { PERSON_ROUTES } from './features/person/person.routes';
+import { PERSON_ROUTES } from './person/person.routes';
 
 export const routes: Routes = [
     ...PERSON_ROUTES
