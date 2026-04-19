f17fd723ab0ddc42ca5cc3cbc8cc414eb7c810a3
unknown <Admin@IT-Antor.bdjobs.local>
2026-04-19 17:50:49 +0600

    Refactor: DTOs split into request/response models

---
+++ FirstProject/DTO/Request/UpdatePersonDataRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTO.Request
+{
+    public class UpdatePersonDataRequest
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [Length(2, 100, ErrorMessage = "Name must be between 2 to 100 characters.")]
+        public string PersonName { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        [DataType(DataType.Date)]
+        public DateOnly PersonDateOfBirth { get; set; }
+        [Required(ErrorMessage = "Height is required.")]
+        [Range(1, 10, ErrorMessage = "Height must be from 1 feet to 10 feet")]
+        public decimal PersonHeightInFeet { get; set; }
+
+        [Required(ErrorMessage = "Weight is required.")]
+        [Range(1, 500, ErrorMessage = "Weight must be from 1 feet to 500 kg")]
+        public decimal PersonWeightInKg { get; set; }
+        [Required(ErrorMessage = "Gender is required.")]
+        public string PersonGender { get; set; }
+
+        [Required(ErrorMessage = "Marital Status is required.")]
+        public string PersonMaritalStatus { get; set; }
+
+        [Required(ErrorMessage = "Graduation Status is required.")]
+        public bool PersonIsGraduated { get; set; }
+    }
+}
---
+++ FirstProject/DTO/Response/GetPersonDataResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace DTO.Response
+{
+    public class GetPersonDataResponse
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public DateOnly PersonDateOfBirth { get; set; }
+        public decimal PersonHeightInFeet { get; set; }
+        public decimal PersonWeightInKg { get; set; }
+        public string PersonGender { get; set; }
+        public string PersonMaritalStatus { get; set; }
+        public bool PersonIsGraduated { get; set; }
+    }
+}
