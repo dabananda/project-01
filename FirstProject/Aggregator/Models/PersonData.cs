546890ea40c61ac1972ea66dcdf0d35e911b5771
dabananda <dabananda.dev@gmail.com>
2026-04-26 15:50:37 +0600

    Remove EF Core dependencies and data annotations

--- FirstProject/Aggregator/Models/PersonData.cs
+++ FirstProject/Aggregator/Models/PersonData.cs
@@ -1,35 +1,16 @@
 ﻿using Aggregator.Enums;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
 
 namespace FirstProject.Models
 {
     public class PersonData
     {
         public int Id { get; set; }
-
-        [Required]
-        [Length(2, 100)]
         public string Name { get; set; }
-
-        [Required]
         public DateOnly DateOfBirth { get; set; }
-
-        [Required]
-        [Precision(3, 2)]
         public decimal HeightInFeet { get; set; }
-
-        [Required]
-        [Precision(5, 2)]
         public decimal WeightInKg { get; set; }
-
-        [Required]
         public Gender Gender { get; set; }
-
-        [Required]
         public MaritalStatus MaritalStatus { get; set; }
-
-        [Required]
         public bool IsGraduated { get; set; }
         public bool IsDeleted { get; set; } = false;
     }
